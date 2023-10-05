91dbaa7985fc6e336766f4b6d5950227f44a6c8f
Aisyah A Fitriani <aisyahafitriani05@gmail.com>
2023-10-05 22:02:00 +0700

    Activity Layout

---
+++ app/src/main/java/com/example/projectlayout/Linear_Layout.kt
@@ -0,0 +1,11 @@
+package com.example.projectlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Linear_Layout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout)
+    }
+}
